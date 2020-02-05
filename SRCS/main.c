54192570330f64761018f6b60a17e7fd4761ab0e
elloimbrandon <elloimbrandon@gmail.com>
2020-02-04 17:01:33 -0800

    .

--- SRCS/main.c
+++ SRCS/main.c
@@ -3,15 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: brfeltz <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: brfeltz <brfeltz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/04 15:39:14 by brfeltz           #+#    #+#             */
-/*   Updated: 2020/02/04 15:42:15 by brfeltz          ###   ########.fr       */
+/*   Updated: 2020/02/04 17:01:16 by brfeltz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+** Reference (https://lodev.org/cgtutor/raycasting.html)
+*/ 
 
 #include "../HEADERS/ft_wolf.h"
 
 int		main(int argc, char **argv)
 {
+
 }
